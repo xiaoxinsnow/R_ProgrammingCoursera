d70aff805846dafc913625970c48208ef9a8c83d
xiaoxinsnow <xiaoxinsnow@hotmail.com>
2015-08-23 00:57:03 -0700

    Add files for Assignment2

---
+++ Assignment2.R
@@ -0,0 +1,54 @@
+setwd("C:/Users/wangx118/Dropbox/Learning and Job/Coursera/Data Science/2. R Programming/Assignments")
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse() 
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+## This is for Coursera Data Science Course 2 R Programming Assignment2, for more information, please refer to the website
